af756f793ff87bc6f1a7e08605ee164565261268
Andre de Cavaignac <laholmes@hotmail.co.uk>
2017-08-22 12:06:03 +0100

    add package config

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pycats',
-    version='0.1',
+    version='0.1.1',
     scripts=['lump.py'],
     description='tools for working with categories in pandas dataframes',
     url='https://github.com/laholmes/pycats',
