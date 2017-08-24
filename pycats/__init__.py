5f2ff3bd40c63817fe551e8bddb81a7e3851de7d
Andre de Cavaignac <laholmes@hotmail.co.uk>
2017-08-24 15:13:17 +0100

    cleaning params for cat_other, cleaning comments

--- pycats/__init__.py
+++ pycats/__init__.py
@@ -1,18 +1,3 @@
-# __all__ = []
-
-# import pkgutil
-# import inspect
-
-# for loader, name, is_pkg in pkgutil.walk_packages(__path__):
-#     module = loader.find_module(name).load_module(name)
-
-#     for name, value in inspect.getmembers(module):
-#         if name.startswith('__'):
-#             continue
-
-#         globals()[name] = value
-#         __all__.append(name)
-
 from src import *
 
 __all__ = ['cat_anon', 'cat_lump', 'as_cat', 'cat_collapse',
--- pycats/other.py
+++ pycats/other.py
@@ -1,2 +1,2 @@
-def cat_other(f, keep, drop, other_level = 'Other'):
+def cat_other(f, drop, other_level = 'Other'):
 	return f.apply(lambda row: row if row in drop else other_level)
