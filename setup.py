604e5fe2985daef772ed52c6e7c036038acc53f1
Andre de Cavaignac <laholmes@hotmail.co.uk>
2017-08-26 16:14:54 +0100

    releasing 0.1.20 cat_lump ties handling

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pycats',
     packages=['pycats'],
-    version='0.1.19',
+    version='0.1.20',
     description='tools for working with categories in pandas dataframes',
     url='https://github.com/laholmes/pycats',
     author='laurence holmes',
